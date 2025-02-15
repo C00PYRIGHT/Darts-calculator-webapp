32cb41e505c37335653311da6cf7042c37ecbfe9
C00PYRIGHT <159134888+C00PYRIGHT@users.noreply.github.com>
2025-02-15 13:35:51 +0100

    Aktuális kör form készen

---
+++ script.js
@@ -0,0 +1,36 @@
+const counter = document.getElementById('pointcounter');
+const gameTypeInput = document.getElementById('Gametype');
+const firstElement = document.getElementById("Firstthrow");
+const thirdElement = document.getElementById("Thirdthrow");
+const secondElement = document.getElementById("Secondthrow");
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+console.log("loaded");
+
+for (let i = 0; i <= 20; i++) {
+    let option1 = document.createElement("option");
+    option1.value = i;
+    option1.textContent = i;
+    if (i === 0) {
+        option1.selected = true;
+    }
+
+    let option2 = option1.cloneNode(true); // Másolat létrehozása
+    let option3 = option1.cloneNode(true); // Még egy másolat
+
+    firstElement.appendChild(option1);
+    secondElement.appendChild(option2);
+    thirdElement.appendChild(option3);
+
+}
+
+});
+document.getElementById('gameTypeButton').onclick = (evt => {
+    evt.preventDefault();
+    console.log('click');
+    let maxpoint = gameTypeInput.value;
+    counter.innerHTML = maxpoint;
+    
+});
